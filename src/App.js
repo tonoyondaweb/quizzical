57b84f66174944313749187e44ba28898addb745
tanay <iamtanaylodh@gmail.com>
2022-09-10 20:01:21 +0530

    added ability to render quiz questions

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import { useState, useEffect } from "react";
 import Question from "./components/Question";
 
 export default function App() {
-	const [start, setStart] = useState(true);
-	// const [quizData, setQuizData] = useState({});
+	const [start, setStart] = useState(false);
+	const [quizData, setQuizData] = useState([
+		{
+			question: "",
+			correct_answer: "",
+			incorrect_answers: [""],
+		},
+	]);
 
 	useEffect(() => {
-		fetch("https://opentdb.com/api.php?amount=10&difficulty=easy")
+		fetch("https://opentdb.com/api.php?amount=5&difficulty=easy")
 			.then((res) => res.json())
-			.then((data) => console.log(data));
+			.then((data) => setQuizData(data.results));
 	}, []);
 
+	// console.log(quizData[0])
+	// const que = quizData[0]
+	const questionEls = quizData.map((question, index) => {
+		const q = question.question;
+		const options = [...question.incorrect_answers, question.correct_answer]
+			.map((value) => ({ value, sort: Math.random() }))
+			.sort((a, b) => a.sort - b.sort)
+			.map(({ value }) => value);
+
+		return (
+			<Question
+				key={index + 1}
+				question={q}
+				options={options}
+				correctAnswer={question.correct_answer}
+			/>
+		);
+	});
+	// console.log(questionEls);
+
 	function startGame() {
-		setStart(true);
+		setStart(prevState => !prevState);
 	}
 
 	return (
@@ -28,9 +54,15 @@ export default function App() {
 					</button>
 				</main>
 			) : (
-				<main className="quiz">
-          <Question />
-        </main>
+				<div>
+					<main className="quiz">{questionEls}</main>
+					<footer className="footer">
+          <h2 className="score">You scored 5/5 correct answers</h2>
+          <button className="restart-button" onClick={startGame}>
+						Play again
+					</button>
+          </footer>
+				</div>
 			)}
 		</div>
 	);
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,28 @@
-export default function Question() {
-    return(
-        <div className="question">
-            <h1>Question</h1>
-        </div>
-    )
+export default function Question(props) {
+	const optionEls = props.options.map((option, index) => {
+		const optionNumber = index + 1;
+		const color = {
+			backgroundColor:
+				option === props.correctAnswer ? "#94D7A2" : "transparent",
+			border: option === props.correctAnswer ? "none" : "1px solid #4D5B9E",
+		};
+		return (
+			<p
+				className="option"
+				key={optionNumber}
+				option={optionNumber - 1}
+				style={color}
+			>
+				{option}
+			</p>
+		);
+	});
+
+	return (
+		<div className="question">
+			<h2>{props.question}</h2>
+			<div className="options">{optionEls}</div>
+			<hr className="divider" />
+		</div>
+	);
 }
