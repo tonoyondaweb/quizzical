f50a431f7d5e38aa0bc3092d5047194320bce8e1
tanay <iamtanaylodh@gmail.com>
2022-09-10 12:04:19 +0530

    built 'start quiz' page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { useState, useEffect } from "react";
+import Question from "./components/Question";
+
 export default function App() {
-  return (
-    <h1>Hello World!</h1>
-  );
+	const [start, setStart] = useState(true);
+	// const [quizData, setQuizData] = useState({});
+
+	useEffect(() => {
+		fetch("https://opentdb.com/api.php?amount=10&difficulty=easy")
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	}, []);
+
+	function startGame() {
+		setStart(true);
+	}
+
+	return (
+		<div className="app">
+			<img className="blob-right" src="../assets/blob-right.svg" alt="" />
+			<img className="blob-left" src="../assets/blob-left.svg" alt="" />
+			{!start ? (
+				<main className="start">
+					<h1>Quizzical</h1>
+					<p>{`{placeholder description}`}</p>
+					<button className="start-button" onClick={startGame}>
+						Start Quiz
+					</button>
+				</main>
+			) : (
+				<main className="quiz">
+          <Question />
+        </main>
+			)}
+		</div>
+	);
 }
---
+++ src/components/Question.js
@@ -0,0 +1,7 @@
+export default function Question() {
+    return(
+        <div className="question">
+            <h1>Question</h1>
+        </div>
+    )
+}
